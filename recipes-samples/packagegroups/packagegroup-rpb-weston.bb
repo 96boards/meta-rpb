96boards/meta-rpb

commit 92f304e9133a1d8c3ce1b14fa1bf0b0552a21f02

rpb*-image: create packagegroup to better organize images
To avoid duplication , let's organize our image using packagegroups. There
should be no change for -desktop and -weston images, but in the process we are
adding more packages to -console image (gps, gstreamer, ...) which are valid
packages even for headless images.

Signed-off-by: Nicolas Dechesne <nicolas.dechesne@linaro.org>

recipes-samples/packagegroups/packagegroup-rpb.bb
@@ -1,0 +1,55 @@
+SUMMARY = "Organize packages to avoid duplication across all images"
+
+inherit packagegroup
+
+PROVIDES = "${PACKAGES}"
+PACKAGES = "\
+    packagegroup-rpb \
+    ${@bb.utils.contains('DISTRO_FEATURES', 'x11', 'packagegroup-rpb-x11', '', d)} \
+    ${@bb.utils.contains('DISTRO_FEATURES', 'wayland', 'packagegroup-rpb-weston', '', d)} \
+    "
+
+# contains basic dependencies, that can work without graphics/display
+RDEPENDS_packagegroup-rpb = "\
+    96boards-tools \
+    alsa-utils-aplay \
+    coreutils \
+    cpufrequtils \
+    gptfdisk \
+    gps-utils \
+    gpsd \
+    gstreamer1.0-plugins-bad-meta \
+    gstreamer1.0-plugins-base-meta \
+    gstreamer1.0-plugins-good-meta \
+    ${@bb.utils.contains("LICENSE_FLAGS_WHITELIST", "commercial_gstreamer1.0-libav", "gstreamer1.0-libav", "", d)} \
+    hostapd \
+    htop \
+    iptables \
+    kernel-modules \
+    networkmanager \
+    networkmanager-nmtui \
+    ${@bb.utils.contains("MACHINE_FEATURES", "optee", "optee-test optee-client", "", d)} \
+    sshfs-fuse \
+    "
+
+SUMMARY_packagegroup-rpb-x11 = "Apps that can be used in X11 Desktop"
+RDEPENDS_packagegroup-rpb-x11 = "\
+    glmark2 \
+    gtkperf \
+    mesa-demos \
+    openbox \
+    openbox-theme-clearlooks \
+    xf86-video-modesetting \
+    xterm \
+    "
+
+SUMMARY_packagegroup-rpb-weston = "Apps that can be used in Weston Desktop"
+RDEPENDS_packagegroup-rpb-weston = "\
+    clutter-1.0-examples \
+    weston \
+    weston-examples \
+    weston-init \
+    ${@bb.utils.contains("MACHINE_FEATURES", "mali450", "mali450-userland", "", d)} \
+    ${@bb.utils.contains("MACHINE_FEATURES", "sgx", "libgbm ti-sgx-ddk-km ti-sgx-ddk-um pru-icss", "", d)} \
+    "
+

recipes-samples/images/rpb-console-image.bb
@@ -10,18 +10,7 @@ inherit core-image distro_features_check extrausers
 REQUIRED_DISTRO_FEATURES = "pam systemd"
 
 CORE_IMAGE_BASE_INSTALL += " \
-    96boards-tools \
-    networkmanager \
-    networkmanager-nmtui \
-    coreutils \
-    cpufrequtils \
-    gptfdisk \
-    hostapd \
-    htop \
-    iptables \
-    kernel-modules \
-    sshfs-fuse \
-    ${@bb.utils.contains("MACHINE_FEATURES", "optee", "optee-test optee-client", "", d)} \
+    packagegroup-rpb \
 "
 
 EXTRA_USERS_PARAMS = "\

recipes-samples/images/rpb-desktop-image.bb
@@ -14,32 +14,8 @@ REQUIRED_DISTRO_FEATURES = "x11 pam systemd"
 SYSTEMD_DEFAULT_TARGET = "graphical.target"
 
 CORE_IMAGE_BASE_INSTALL += " \
-    96boards-tools \
-    alsa-utils-aplay \
-    networkmanager \
-    networkmanager-nmtui \
-    coreutils \
-    cpufrequtils \
-    glmark2 \
-    gps-utils \
-    gpsd \
-    gptfdisk \
-    gstreamer1.0-plugins-bad-meta \
-    gstreamer1.0-plugins-base-meta \
-    gstreamer1.0-plugins-good-meta \
-    ${@bb.utils.contains("LICENSE_FLAGS_WHITELIST", "commercial_gstreamer1.0-libav", "gstreamer1.0-libav", "", d)} \
-    gtkperf \
-    hostapd \
-    htop \
-    iptables \
-    kernel-modules \
-    mesa-demos \
-    openbox \
-    openbox-theme-clearlooks \
-    sshfs-fuse \
-    xf86-video-modesetting \
-    xterm \
-    ${@bb.utils.contains("MACHINE_FEATURES", "optee", "optee-test optee-client", "", d)} \
+    packagegroup-rpb \
+    packagegroup-rpb-x11 \
 "
 
 EXTRA_USERS_PARAMS = "\

recipes-samples/images/rpb-weston-image.bb
@@ -15,29 +15,8 @@ REQUIRED_DISTRO_FEATURES = "wayland pam systemd"
 #   ${@bb.utils.contains('DISTRO_FEATURES', 'wifi', 'wcnss-config', '', d)}
 # http://git.yoctoproject.org/cgit/cgit.cgi/meta-qcom/tree/conf/machine/dragonboard-410c.conf#n37
 CORE_IMAGE_BASE_INSTALL += " \
-    96boards-tools \
-    alsa-utils-aplay \
-    clutter-1.0-examples \
-    networkmanager \
-    networkmanager-nmtui \
-    coreutils \
-    cpufrequtils \
-    gptfdisk \
-    gstreamer1.0-plugins-bad-meta \
-    gstreamer1.0-plugins-base-meta \
-    gstreamer1.0-plugins-good-meta \
-    ${@bb.utils.contains("LICENSE_FLAGS_WHITELIST", "commercial_gstreamer1.0-libav", "gstreamer1.0-libav", "", d)} \
-    hostapd \
-    htop \
-    iptables \
-    kernel-modules \
-    sshfs-fuse \
-    weston \
-    weston-examples \
-    weston-init \
-    ${@bb.utils.contains("MACHINE_FEATURES", "optee", "optee-test optee-client", "", d)} \
-    ${@bb.utils.contains("MACHINE_FEATURES", "mali450", "mali450-userland", "", d)} \
-    ${@bb.utils.contains("MACHINE_FEATURES", "sgx", "libgbm ti-sgx-ddk-km ti-sgx-ddk-um pru-icss", "", d)} \
+    packagegroup-rpb \
+    packagegroup-rpb-weston \
 "
 
 EXTRA_USERS_PARAMS = "\

commit bff707afc62838a486cb782c60fcf252994205c1

packagegroup: split -rpb into multiple recipes
Commit 92f304e9133 (rpb*-image: create packagegroup to better organize image)
introduced a regression, since

* packagegroup-rpb recipe is required for -console image, to all packages from
packagegroup-rpb-x11 are built as well, though they are not included in the
image.

* the commit also adds Gstreamer in the -console image. When building -console
with 'x11' enabled, it will bring a lot of X packages regardless of
packagegroup-rpb-x11

As such, we are now doing splitting the packagegroup recipes into multiple
recipes to avoid building packages not needed when building only -console
image, and we remove Gstreamer from -console for now.

Signed-off-by: Nicolas Dechesne <nicolas.dechesne@linaro.org>

recipes-samples/packagegroups/packagegroup-rpb-weston.bb
@@ -1,0 +1,21 @@
+SUMMARY = "Organize packages to avoid duplication across all images (weston)"
+
+inherit packagegroup distro_features_check
+REQUIRED_DISTRO_FEATURES = "wayland"
+
+SUMMARY_packagegroup-rpb-weston = "Apps that can be used in Weston Desktop"
+RDEPENDS_packagegroup-rpb-weston = "\
+    clutter-1.0-examples \
+    gps-utils \
+    gpsd \
+    gstreamer1.0-plugins-bad-meta \
+    gstreamer1.0-plugins-base-meta \
+    gstreamer1.0-plugins-good-meta \
+    ${@bb.utils.contains("LICENSE_FLAGS_WHITELIST", "commercial_gstreamer1.0-libav", "gstreamer1.0-libav", "", d)} \
+    weston \
+    weston-examples \
+    weston-init \
+    ${@bb.utils.contains("MACHINE_FEATURES", "mali450", "mali450-userland", "", d)} \
+    ${@bb.utils.contains("MACHINE_FEATURES", "sgx", "libgbm ti-sgx-ddk-km ti-sgx-ddk-um pru-icss", "", d)} \
+    "
+

recipes-samples/packagegroups/packagegroup-rpb-x11.bb
@@ -1,0 +1,21 @@
+SUMMARY = "Organize packages to avoid duplication across all images (with X11)"
+
+inherit packagegroup distro_features_check
+REQUIRED_DISTRO_FEATURES = "x11"
+
+SUMMARY_packagegroup-rpb-x11 = "Apps that can be used in X11 Desktop"
+RDEPENDS_packagegroup-rpb-x11 = "\
+    glmark2 \
+    gps-utils \
+    gpsd \
+    gstreamer1.0-plugins-bad-meta \
+    gstreamer1.0-plugins-base-meta \
+    gstreamer1.0-plugins-good-meta \
+    ${@bb.utils.contains("LICENSE_FLAGS_WHITELIST", "commercial_gstreamer1.0-libav", "gstreamer1.0-libav", "", d)} \
+    gtkperf \
+    mesa-demos \
+    openbox \
+    openbox-theme-clearlooks \
+    xf86-video-modesetting \
+    xterm \
+    "

recipes-samples/packagegroups/packagegroup-rpb.bb
@@ -2,13 +2,6 @@ SUMMARY = "Organize packages to avoid duplication across all images"
 
 inherit packagegroup
 
-PROVIDES = "${PACKAGES}"
-PACKAGES = "\
-    packagegroup-rpb \
-    ${@bb.utils.contains('DISTRO_FEATURES', 'x11', 'packagegroup-rpb-x11', '', d)} \
-    ${@bb.utils.contains('DISTRO_FEATURES', 'wayland', 'packagegroup-rpb-weston', '', d)} \
-    "
-
 # contains basic dependencies, that can work without graphics/display
 RDEPENDS_packagegroup-rpb = "\
     96boards-tools \
@@ -17,12 +10,6 @@ RDEPENDS_packagegroup-rpb = "\
     cpufrequtils \
     docker \
     gptfdisk \
-    gps-utils \
-    gpsd \
-    gstreamer1.0-plugins-bad-meta \
-    gstreamer1.0-plugins-base-meta \
-    gstreamer1.0-plugins-good-meta \
-    ${@bb.utils.contains("LICENSE_FLAGS_WHITELIST", "commercial_gstreamer1.0-libav", "gstreamer1.0-libav", "", d)} \
     hostapd \
     htop \
     iptables \
@@ -33,25 +20,3 @@ RDEPENDS_packagegroup-rpb = "\
     rsync \
     sshfs-fuse \
     "
-
-SUMMARY_packagegroup-rpb-x11 = "Apps that can be used in X11 Desktop"
-RDEPENDS_packagegroup-rpb-x11 = "\
-    glmark2 \
-    gtkperf \
-    mesa-demos \
-    openbox \
-    openbox-theme-clearlooks \
-    xf86-video-modesetting \
-    xterm \
-    "
-
-SUMMARY_packagegroup-rpb-weston = "Apps that can be used in Weston Desktop"
-RDEPENDS_packagegroup-rpb-weston = "\
-    clutter-1.0-examples \
-    weston \
-    weston-examples \
-    weston-init \
-    ${@bb.utils.contains("MACHINE_FEATURES", "mali450", "mali450-userland", "", d)} \
-    ${@bb.utils.contains("MACHINE_FEATURES", "sgx", "libgbm ti-sgx-ddk-km ti-sgx-ddk-um pru-icss", "", d)} \
-    "
-