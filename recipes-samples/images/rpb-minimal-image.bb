96boards/meta-rpb

commit ceaef9214090cf40a2053aa8a686d471a50b316b

rpb-minimal-image: replace debug-tweaks feature
The `debug-tweaks` IMAGE_FEATURE has been dropped in favour of
explicitly using corresponding features. Replace it with the suggested
list of features.

Signed-off-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>

recipes-samples/images/rpb-minimal-image.bb
@@ -1,6 +1,6 @@
 SUMMARY = "Minimal image"
 
-IMAGE_FEATURES += "splash tools-debug debug-tweaks enable-adbd"
+IMAGE_FEATURES += "splash tools-debug allow-empty-password empty-root-password allow-root-login post-install-logging"
 
 LICENSE = "MIT"
 

commit 9b2cb7773b3b123b924f50f318bea99ef6a5257a

rpb-minimal-image: restore enable-adbd feature
Bring back the "enable-adbd" feature dropped in the last commit.

Fixes: ceaef9214090 ("rpb-minimal-image: replace debug-tweaks feature")
Signed-off-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>

recipes-samples/images/rpb-minimal-image.bb
@@ -1,6 +1,6 @@
 SUMMARY = "Minimal image"
 
-IMAGE_FEATURES += "splash tools-debug allow-empty-password empty-root-password allow-root-login post-install-logging"
+IMAGE_FEATURES += "splash tools-debug allow-empty-password empty-root-password allow-root-login post-install-logging enable-adbd"
 
 LICENSE = "MIT"
 